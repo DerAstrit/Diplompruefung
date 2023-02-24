9af901f476af46a87362c9d82be55a8aacd4b48e
DerAstrit <68617105+DerAstrit@users.noreply.github.com>
2023-02-24 19:44:25 +0100

    Update CrawlManager.cs

--- PROG/Webcrawler/CrawlManager.cs
+++ PROG/Webcrawler/CrawlManager.cs
@@ -1,1 +1,25 @@
+namespace Webcrawler;
 
+
+public class CrawlManager
+{
+
+
+    public void Start()
+    {
+        InputOptions inputOptions = new InputOptions();
+        Intro();
+        inputOptions.WriteOptions();
+        inputOptions.SelectOptions();
+    }
+    
+      
+    private void Intro()
+    {
+        Console.WriteLine("\nWelcome to the Webcrawler \n" +
+                          "This Tool is for learn purposes and should not be abused \n" +
+                          "THANKS!\n" +
+                          "");
+    }
+
+}
